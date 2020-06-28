6590d3475b12f8068c4bc7da11740e0c2f41e6e6
jefersonknop <jefersonknop@gmail.com>
2020-06-28 17:42:55 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+
+//prime ng
+import {AccordionModule} from 'primeng/accordion'; 
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserModule,
+    AppRoutingModule,
+
+    //primeng
+    AccordionModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
